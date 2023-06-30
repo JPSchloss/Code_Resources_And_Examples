34164efb480a448cdbdacc2007f14f8262242afc
Jonathan Schlosser <50070349+JPSchloss@users.noreply.github.com>
2023-06-30 12:02:29 -0400

    Adding Simple ML Model Examples

---
+++ Machine Learning/Simple_Log_Regression.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+
+# Get the data
+X = np.random.rand(100, 2)  # Input features
+y = np.random.randint(0, 2, size=100)  # Target labels (binary)
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create the ML model
+model = LogisticRegression()
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Check the model
+report = classification_report(y_test, y_pred)
+print(report)
