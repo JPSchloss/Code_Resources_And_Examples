5cdf94d2151716d6c194a068064ef7913cb81ea1
Jonathan Schlosser <50070349+JPSchloss@users.noreply.github.com>
2023-06-29 20:58:44 -0400

    Matplotlib and Seaborn Correlation Matrix Approach Comparison

---
+++ Comparing_Corr_Matrix_Matplotlib_Seaborn.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+# Generate random correlation data
+def generate_data():
+    np.random.seed(42)
+    data = np.random.randn(5, 5)
+    return data
+
+# Create a correlation matrix plot with Matplotlib
+def matplotlib_corr_matrix(data):
+    fig, ax = plt.subplots()
+    cax = ax.matshow(data, cmap='coolwarm')
+    fig.colorbar(cax)
+    
+    # Add correlation values as cell labels
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            plt.text(j, i, f'{data[i, j]:.2f}', ha='center', va='center', color='black')
+    
+    plt.xticks(range(data.shape[1]))
+    plt.yticks(range(data.shape[0]))
+    plt.title("Correlation Matrix - Matplotlib")
+    return plt.show()
+
+# Create a correlation matrix plot with Seaborn
+def seaborn_corr_matrix(data):
+    sns.heatmap(data, cmap='coolwarm', annot=True, square=True)
+    plt.title("Correlation Matrix - Seaborn")
+    return plt.show()
+
+# Creating a main function to generate the data and run through the plotting approaches. 
+def main():
+    data = generate_data()
+    matplotlib_corr_matrix(data)
+    seaborn_corr_matrix(data)
+    return
+
+main()
