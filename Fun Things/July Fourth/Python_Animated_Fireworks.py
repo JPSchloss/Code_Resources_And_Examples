eafa10a22867f0b97b3f36ad6087d3e8ae177043
Jonathan Schlosser <50070349+JPSchloss@users.noreply.github.com>
2023-07-02 07:04:05 -0400

    Updating Fireworks Script

--- Fun Things/July Fourth/Python_Animated_Fireworks.py
+++ Fun Things/July Fourth/Python_Animated_Fireworks.py
@@ -1,15 +1,21 @@
 import os, time
 
+# Set the ASCII Art Filenames
 filenames = ['Fireworks_1.txt', 'Fireworks_2.txt', 'Fireworks_3.txt']
 
+# Define function for animation
 def animation_run(filenames, delay = .5, repetition = 10):
+
+    # Clear the screen and create an object to hold the ASCII files.
     os.system('clear')
     frames = []
 
+    # Loop through the files and load them into the environment. 
     for name in filenames:
         with open(name, 'r', encoding='utf8') as f:
             frames.append(f.readlines())
 
+    # Loop through a range and each image to create the animation. 
     for i in range(repetition):
         for frame in frames:
             print("".join(frame))
